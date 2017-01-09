7b6528e8fb3882b624bc6655781032a3c5b16a68
kakalak-lumberJack <jwmannin@gmail.com>
2017-01-08 20:31:53 -0500

    Add files via upload

--- mapgen.lua
+++ mapgen.lua
@@ -6,9 +6,9 @@ minetest.register_ore({
       y_max = -128,
       noise_threshold = 0.7,
       noise_params = {
-              offset = 0,
+              offset = -0.16,
               scale = 1,
-              spread = {x = 55, y = 55, z = 55},
+              spread = {x = 25, y = 25, z = 25},
               seed = 23,
               octaves = 3,
               persist = 0.70,
@@ -26,9 +26,9 @@ minetest.register_ore({
       y_max = -256,
       noise_threshold = 0.7,
       noise_params = {
-              offset = 0,
+              offset = -0.16,
               scale = 1,
-              spread = {x = 55, y = 55, z = 55},
+              spread = {x = 25, y = 25, z = 25},
               seed = 23,
               octaves = 3,
               persist = 0.70,
@@ -45,9 +45,9 @@ minetest.register_ore({
       y_max = -512,
       noise_threshold = 0.7,
       noise_params = {
-              offset = 0,
+              offset = -0.16,
               scale = 1,
-              spread = {x = 55, y = 55, z = 55},
+              spread = {x = 25, y = 25, z = 25},
               seed = 23,
               octaves = 3,
               persist = 0.70,
@@ -63,8 +63,8 @@ minetest.register_ore({
         clust_scarcity = 14 * 14 * 14,
         clust_num_ores = 3,
         clust_size = 2,
-        y_min = -255,
-        y_max = -64,
+        y_min = -512,
+        y_max = -128,
 
 })
 
@@ -76,7 +76,7 @@ minetest.register_ore({
         clust_num_ores = 5,
         clust_size = 3,
         y_min = -31000,
-        y_max = -256,
+        y_max = -512,
 })
 
 minetest.register_ore({
